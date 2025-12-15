1117c7dd7ec0524e31bf7636a2709e21f98632fa
goswami22 <goswamibhavesh22@gmail.com>
2025-12-15 22:14:47 +0530

    New changes added

--- Tops-Traning/Back-end/C/Tasks/averag.c
+++ Tops-Traning/Back-end/C/Tasks/averag.c
@@ -19,12 +19,22 @@ int main() {
 	printf("Enter the Gujarati Value here:\n");
 	scanf("%d",&Gujarati);
 
-	int avg = (Maths + English + Science + Gujarati) / 4;
-	printf("The average number is %d\n", avg);
+	float avg = (Maths + English + Science + Gujarati) / 4;
+	printf("The average number is %f\n", avg);
 
 	float per = ((Maths + English + Science + Gujarati) * 100.0) / 400;
-	printf("The percentage of Total marks is %.2f", per);
+	printf("The percentage of Total marks is %.2f%% \n", per);
 
+	
+	if(per >= 80 && per <= 100) {
+		printf("Student Grade is A \n");
+	} else if(per >= 60 && per < 80){
+		printf("Student Grade is B \n");
+	} else if(per >= 40 && per < 60){
+		printf("Student Grade is C \n");
+	} else {
+		printf("You are fail");
+	}
 
 
 	return 0;
