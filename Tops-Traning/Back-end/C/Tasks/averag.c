c00fb41b6e4ea7abf2e47efcaee86bde3568d360
goswami22 <goswamibhavesh22@gmail.com>
2025-12-15 19:01:51 +0530

    new file added

---
+++ Tops-Traning/Back-end/C/Tasks/averag.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main() {
+
+	//	 average number
+
+	int Maths, English , Science , Gujarati;
+
+	printf("Enter the Maths Value here:\n");
+	scanf("%d", &Maths);
+
+	printf("Enter the English Value here:\n");
+	scanf("%d", &English);
+
+	printf("Enter the Scince Value here:\n");
+	scanf("%d", &Science);
+	
+	printf("Enter the Gujarati Value here:\n");
+	scanf("%d",&Gujarati);
+
+	int avg = (Maths + English + Science + Gujarati) / 4;
+	printf("The average number is %d\n", avg);
+
+	float per = ((Maths + English + Science + Gujarati) * 100.0) / 400;
+	printf("The percentage of Total marks is %.2f", per);
+
+
+
+	return 0;
+}
