6195de839dda965eb14aa60516be5c9aec56b6c8
goswami22 <goswamibhavesh22@gmail.com>
2025-12-16 10:09:05 +0530

    new file added

---
+++ Tops-Traning/Back-end/C/Tasks/03_nested_if.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(){
+	
+	
+	int a, b, c;
+	
+	printf("Enter a value: \n");
+	scanf("%d", &a);
+	
+	printf("Enter b value: \n");
+	scanf("%d", &b);
+	
+	printf("Enter c value: \n");
+	scanf("%d", &c);
+	
+
+	
+	
+	return 0;
+}
