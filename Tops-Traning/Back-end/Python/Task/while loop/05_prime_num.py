f3688bf6a7ae0c1fe1d486479fea78f4849f4adf
goswami22 <goswamibhavesh22@gmail.com>
2026-01-22 21:08:57 +0530

    22-1-2026

--- Tops-Traning/Back-end/Python/Task/while loop/05_prime_num.py
+++ Tops-Traning/Back-end/Python/Task/while loop/05_prime_num.py
@@ -5,4 +5,14 @@
 
 n = int (input("Enter Number: "))
 
-
+if n > 1:
+    for i in range(2 , n):
+        if n % i == 0:
+            print("Not a prime number")
+            break
+        else:
+            print("Prime number")
+            
+    else:
+        print("NOt prime")
+        
