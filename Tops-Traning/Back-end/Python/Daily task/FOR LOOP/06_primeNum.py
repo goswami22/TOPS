e616bb9b85145081d90ba9b48029759b58fba8bb
goswami22 <goswamibhavesh22@gmail.com>
2026-01-23 22:50:52 +0530

    23-1-2026

--- Tops-Traning/Back-end/Python/Daily task/FOR LOOP/06_primeNum.py
+++ Tops-Traning/Back-end/Python/Daily task/FOR LOOP/06_primeNum.py
@@ -1,10 +1,10 @@
 num = int(input("Enter Numebr: "))
 
-if num%2!=0:
-    for i in range(3, int(num/2)+1,2):
-        if num%i == 0:
-            print(num,"Not Prime Number")
-            break
+if num%2!=0:                                    # get odd numbers
+    for i in range(3, int(num/2)+1,2):          # start form 3 and 
+        if num%i == 0:                          # check number is devided by zero then not prime  i menas starting number
+            print(num,"Not Prime Number")       
+            break                               # if number not prime then stop 
     else:
         print(num, "Prime Number")
 else:
---
+++ Tops-Traning/Back-end/Python/Daily task/String/05_string.py
@@ -0,0 +1,29 @@
+s = ('Bhavesh Goswamsi')
+
+print(s.upper())                    
+
+print(s.lower())
+
+print(s.capitalize())
+
+print(s.title())
+
+print(s.casefold())
+
+print(s.center(40, "="))
+
+print(s.endswith("ami"))
+
+print(s.startswith("Go"))
+
+print(s.find('ve'))
+
+print(s.islower())
+
+print(s.isupper())
+
+print(s.istitle())
+
+print(s.count('s'))
+
+print(s.isalnum())
